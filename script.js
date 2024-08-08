826d1aea6d4881364d3d47595f5e13d10356b5fc
Leyla <radlejla5@gmail.com>
2024-08-08 17:57:25 +0500

    hw

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = prompt('Введите имя')
+let about = "Лейла "
+console.log(about)
+
+let num = prompt("Введите возраст")
+let year = "22 года"
+console.log(year)
+
+let country = prompt("Укажите город где проживаете")
+let city = "Самарканд"
+console.log(city)
